6780d66cd8704fa3bb8016e383ba2d7bc01bedf7
guigs10mil <gui.m.s@hotmail.com>
2016-09-21 09:21:58 -0300

    Entrega do dia 21/09

--- Portifólio de Projetos/js/ani.js
+++ Portifólio de Projetos/js/ani.js
@@ -22,39 +22,35 @@ $(document).ready(function () {
     $('.carousel').carousel();
 });
 
-var options = [
-    {
-        selector: 'li', offset: 200, callback: function(el) {
-            Materialize.accordionOpen($(el));
-        } 
-    },
-];
-Materialize.scrollFire(options);
-
-$(document).ready(function () {
-
-    var owl = $("#owl-demo");
-
-    owl.owlCarousel({
-        items: 1, //10 items above 1000px browser width
-        itemsDesktop: [200, 1], //5 items between 1000px and 901px
-        itemsDesktopSmall: [200, 1], // betweem 900px and 601px
-        itemsTablet: [200, 1], //2 items between 600 and 0
-        itemsMobile: true // itemsMobile disabled - inherit from itemsTablet option
-    });
-
-    // Custom Navigation Events
-    $(".next").click(function () {
-        owl.trigger('owl.next');
-    })
-    $(".prev").click(function () {
-        owl.trigger('owl.prev');
-    })
-    $(".play").click(function () {
-        owl.trigger('owl.play', 1000); //owl.play event accept autoPlay speed as second parameter
-    })
-    $(".stop").click(function () {
-        owl.trigger('owl.stop');
-    })
-
+$('.carousel').carousel({
+    padding: 200    
 });
+autoplay()   
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+
+/*
+function pullDown() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+*/
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
